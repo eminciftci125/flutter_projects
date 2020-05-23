243de1f68a1e368dc7d1fd42f7aac7792f62ee1f
eminciftci125 <eminciftci123@gmail.com>
2020-05-23 16:45:33 +0300

    el yüz düzeltme

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,9 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:keyboard_visibility/keyboard_visibility.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({
     Key key,
   }) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool hideRegister = false;
+
+  @protected
+  void initState() {
+    super.initState();
+
+    KeyboardVisibilityNotification().addNewListener(
+      onChange: (bool visible) {
+        if (visible)
+          setState(() {
+            hideRegister = true;
+          });
+        else
+          setState(() {
+            hideRegister = false;
+          });
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +44,11 @@ class Login extends StatelessWidget {
               labelText: "Kullanıcı Adı",
               filled: true,
               fillColor: Colors.white,
-              border: OutlineInputBorder(),
+              border: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(30.0),
+                ),
+              ),
             ),
           ),
           SizedBox(
@@ -31,26 +61,37 @@ class Login extends StatelessWidget {
               labelText: "Şifre",
               filled: true,
               fillColor: Colors.white,
-              border: OutlineInputBorder(),
+              border: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(30.0),
+                ),
+              ),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 15.0),
             child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              ),
               color: Colors.white,
               onPressed: () {},
               child: Text('Giriş Yap'),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50.0),
-            child: FlatButton(
-              onPressed: () {},
-              child: Text(
-                'Kaydol',
-              ),
-            ),
-          ),
+          hideRegister == false
+              ? Container(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Kaydol',
+                      ),
+                    ),
+                  ),
+                )
+              : Container(),
         ],
       ),
     );
